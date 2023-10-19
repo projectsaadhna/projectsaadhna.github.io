392d68f09c868b68e1827a743d41bc5ef6e10449
Vignesh Nagavel <72061699+vigneshnagavel@users.noreply.github.com>
2023-10-19 16:25:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import MouseFollower from "./Components/MouseFollower"
 
 function App() {
   return (
-    <div className="h-[100vh] flex justify-center items-center  bg-[#123457]">
-      <img className = "fixed move1 lg:top-[80vh] lg:left-[10vw] top-[90vh] left-[20vw]" src = {blue} alt = ""/>
+    <div className="h-[100vh] flex justify-center items-center  bg-gradient-to-br from-[#ffffff] to-[#ffffff]">
+      <img className = "fixed move1 lg:top-[80vh] lg:left-[10vw] top-[90vh] left-[20vw]" src = {green} alt = ""/>
       <img className = "fixed move2 lg:top-[80vh] lg:left-[20vw] top-[90vh] left-[50vw]" src={yellow} alt = "" />
       <img className = "fixed move3 lg:bottom-[80vh] lg:right-[20vw] bottom-[90vh] right-[20vw]" src = {red} alt = "" />
-      <img className = "fixed move4 lg:bottom-[80vh] lg:right-[10vw] bottom-[90vh] right-[50vw]" src = {green} alt = "" />
+      <img className = "fixed move4 lg:bottom-[80vh] lg:right-[10vw] bottom-[90vh] right-[50vw]" src = {blue} alt = "" />
       <Content className="" />
       <MouseFollower />
     </div>
